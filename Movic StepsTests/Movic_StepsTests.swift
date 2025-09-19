26f526a88e4cb56588704fa1fb046fcc5fba4042
AirforceRP <lucsun.game@gmail.com>
2025-09-19 16:32:20 -0400

    Initial Commit

---
+++ Movic StepsTests/Movic_StepsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movic_StepsTests.swift
+//  Movic StepsTests
+//
+//  Created by Luc Sun on 9/19/25.
+//
+
+import Testing
+@testable import Movic_Steps
+
+struct Movic_StepsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
